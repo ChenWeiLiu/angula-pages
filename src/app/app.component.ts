57d5090582af7e37c597bbb2a467f1c87e988022
chenwei liu <a500667337@gmail.com>
2020-04-23 01:21:35 +0800

    練離完成

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,154 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pages';
+  currentPage = 0;
+  images = [
+    {
+      title: '海灘',
+      url: 'https://images.unsplash.com/photo-1568515464361-565053db3b20?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: '森林',
+      url: 'https://images.unsplash.com/photo-1584352278525-b0f2872cd10a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: '太陽',
+      url: 'https://images.unsplash.com/photo-1574250813071-215b287785ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: '食物',
+      url: 'https://images.unsplash.com/photo-1541696432-82c6da8ce7bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: '海灘',
+      url: 'https://images.unsplash.com/photo-1568515464361-565053db3b20?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: '森林',
+      url: 'https://images.unsplash.com/photo-1584352278525-b0f2872cd10a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: '太陽',
+      url: 'https://images.unsplash.com/photo-1574250813071-215b287785ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: '食物',
+      url: 'https://images.unsplash.com/photo-1541696432-82c6da8ce7bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: '海灘',
+      url: 'https://images.unsplash.com/photo-1568515464361-565053db3b20?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: '森林',
+      url: 'https://images.unsplash.com/photo-1584352278525-b0f2872cd10a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: '太陽',
+      url: 'https://images.unsplash.com/photo-1574250813071-215b287785ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: '食物',
+      url: 'https://images.unsplash.com/photo-1541696432-82c6da8ce7bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: '海灘',
+      url: 'https://images.unsplash.com/photo-1568515464361-565053db3b20?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: '森林',
+      url: 'https://images.unsplash.com/photo-1584352278525-b0f2872cd10a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: '太陽',
+      url: 'https://images.unsplash.com/photo-1574250813071-215b287785ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: '食物',
+      url: 'https://images.unsplash.com/photo-1541696432-82c6da8ce7bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    }, {
+      title: '海灘',
+      url: 'https://images.unsplash.com/photo-1568515464361-565053db3b20?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: '森林',
+      url: 'https://images.unsplash.com/photo-1584352278525-b0f2872cd10a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: '太陽',
+      url: 'https://images.unsplash.com/photo-1574250813071-215b287785ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: '食物',
+      url: 'https://images.unsplash.com/photo-1541696432-82c6da8ce7bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: '海灘',
+      url: 'https://images.unsplash.com/photo-1568515464361-565053db3b20?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: '森林',
+      url: 'https://images.unsplash.com/photo-1584352278525-b0f2872cd10a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: '太陽',
+      url: 'https://images.unsplash.com/photo-1574250813071-215b287785ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: '食物',
+      url: 'https://images.unsplash.com/photo-1541696432-82c6da8ce7bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: '海灘',
+      url: 'https://images.unsplash.com/photo-1568515464361-565053db3b20?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: '森林',
+      url: 'https://images.unsplash.com/photo-1584352278525-b0f2872cd10a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: '太陽',
+      url: 'https://images.unsplash.com/photo-1574250813071-215b287785ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: '食物',
+      url: 'https://images.unsplash.com/photo-1541696432-82c6da8ce7bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: '海灘',
+      url: 'https://images.unsplash.com/photo-1568515464361-565053db3b20?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: '森林',
+      url: 'https://images.unsplash.com/photo-1584352278525-b0f2872cd10a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: '太陽',
+      url: 'https://images.unsplash.com/photo-1574250813071-215b287785ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: '食物',
+      url: 'https://images.unsplash.com/photo-1541696432-82c6da8ce7bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: '海灘',
+      url: 'https://images.unsplash.com/photo-1568515464361-565053db3b20?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: '森林',
+      url: 'https://images.unsplash.com/photo-1584352278525-b0f2872cd10a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: '太陽',
+      url: 'https://images.unsplash.com/photo-1574250813071-215b287785ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: '食物',
+      url: 'https://images.unsplash.com/photo-1541696432-82c6da8ce7bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    }
+  ];
+
+  checkWindowWIndex(index: number) {
+    return Math.abs(this.currentPage - index) < 5;
+  }
 }
